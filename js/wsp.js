4821b7806a713bd11efe4dda606d5d88c75e865d
estefaniaMartin <martinestefania@gmail.com>
2023-05-23 19:09:51 -0300

    se agregó btn-wsp a js y se actualizó ruta de imagenes

---
+++ js/wsp.js
@@ -0,0 +1,8 @@
+let btnWasp = `
+<div class="whatsapp">
+    <a class="btn-wsp" href="https://web.whatsapp.com/send?phone=541158755260&amp;text=Realizar%20una%20consulta."
+    rel="nofollow" target="_blank"><img src="./img/wsp.png" class="whatsapp-icon" alt="wsp">
+    </a>
+</div>
+`
+document.querySelector(".whatsapp").innerHTML = btnWasp;
